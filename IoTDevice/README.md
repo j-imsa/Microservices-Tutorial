be5c38525100672e6aee9096f1eddcf679500978
Iman Salehi <cse.isalehi@gmail.com>
2024-08-26 15:15:17 +0330

    create IoTDevice microservice

---
+++ IoTDevice/README.md
@@ -0,0 +1,1 @@
+# # Microservice #2: IoT Device
---
+++ IoTDevice/src/main/java/be/jimsa/iotdevice/IoTDeviceApplication.java
@@ -0,0 +1,13 @@
+package be.jimsa.iotdevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTDeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTDeviceApplication.class, args);
+    }
+
+}
