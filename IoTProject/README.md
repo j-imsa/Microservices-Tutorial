c75cfe0a2108947f5bb654a1f985f625248e96d4
Iman Salehi <cse.isalehi@gmail.com>
2024-08-26 15:08:52 +0330

    create IoTProject microservice

---
+++ IoTProject/README.md
@@ -0,0 +1,1 @@
+# Microservice #1: IoT Project
---
+++ IoTProject/src/main/java/be/jimsa/iotproject/IoTProjectApplication.java
@@ -0,0 +1,13 @@
+package be.jimsa.iotproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTProjectApplication.class, args);
+    }
+
+}
