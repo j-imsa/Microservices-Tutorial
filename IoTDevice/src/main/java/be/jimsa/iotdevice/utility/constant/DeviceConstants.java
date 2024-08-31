69fcd50c88e90fb7b2386724a4601a768503f340
Iman Salehi <cse.isalehi@gmail.com>
2024-08-31 21:25:11 +0330

    add DeviceConstants

---
+++ IoTDevice/src/main/java/be/jimsa/iotdevice/utility/constant/DeviceConstants.java
@@ -0,0 +1,6 @@
+package be.jimsa.iotdevice.utility.constant;
+
+public class DeviceConstants {
+    public static final String EXCEPTION_CAN_NOT_CREATE = "Can not create a new device";
+    public static final int PUBLIC_ID_LENGTH = 32;
+}
