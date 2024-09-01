606c722ccab8201a4a854e8c7e4d05b9fa09ff46
Iman Salehi <cse.isalehi@gmail.com>
2024-09-01 16:22:41 +0330

    impl validation handler

--- IoTProject/src/main/java/be/jimsa/iotproject/config/exception/handler/AppExceptionHandler.java
+++ IoTProject/src/main/java/be/jimsa/iotproject/config/exception/handler/AppExceptionHandler.java
@@ -7,18 +7,48 @@ import be.jimsa.iotproject.config.exception.ResourceAlreadyExistException;
 import be.jimsa.iotproject.utility.constant.ProjectConstants;
 import be.jimsa.iotproject.ws.model.dto.ResponseDto;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 @RestControllerAdvice
-public class AppExceptionHandler {
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> hashMap = new HashMap<>();
+        List<ObjectError> errors = ex.getBindingResult().getAllErrors();
+
+        errors.forEach(error ->
+                hashMap.put((
+                                (FieldError) error).getField(),
+                        error.getDefaultMessage()
+                )
+        );
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(
+                        ResponseDto.builder()
+                                .action(false)
+                                .timestamp(LocalDateTime.now())
+                                .result(hashMap)
+                                .build()
+                );
+    }
 
     @ExceptionHandler(value = {
             BadFormatRequestException.class,
