dc4c151d67162968b0ac6a699f72c2a84a3ef55d
Iman Salehi <cse.isalehi@gmail.com>
2024-09-02 15:11:23 +0330

    add @DtosDocument

--- IoTProject/src/main/java/be/jimsa/iotproject/ws/model/dto/ProjectDto.java
+++ IoTProject/src/main/java/be/jimsa/iotproject/ws/model/dto/ProjectDto.java
@@ -1,17 +1,20 @@
 package be.jimsa.iotproject.ws.model.dto;
 
 import be.jimsa.iotproject.config.validation.annotation.ValidPublicId;
-import be.jimsa.iotproject.utility.constant.ProjectConstants;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Builder;
 import lombok.Data;
-import org.hibernate.sql.Update;
 
 import static be.jimsa.iotproject.utility.constant.ProjectConstants.*;
 
 @Data
 @Builder
+@Schema(
+        name = "Project",
+        description = "This is 'ProjectDto'"
+)
 public class ProjectDto {
 
     public interface Create {
@@ -32,6 +35,9 @@ public class ProjectDto {
     @ValidPublicId(groups = Read.class)
     @Null(message = VALIDATION_PUBLIC_ID_NULL_MESSAGE,
             groups = {Create.class, PutUpdate.class, PatchUpdate.class})
+    @Schema(
+            description = "read-only field"
+    )
     private String publicId;
 
     @NotEmpty(message = VALIDATION_NAME_NOT_EMPTY_MESSAGE, groups = {Read.class, Create.class, PutUpdate.class})
@@ -40,6 +46,9 @@ public class ProjectDto {
             groups = {Read.class, Create.class, PutUpdate.class})
     @Pattern(regexp = VALIDATION_NAME_PATTERN, message = VALIDATION_NAME_SIZE_MESSAGE,
             groups = PatchUpdate.class)
+    @Schema(
+            description = "name of project", example = "Alpha w1"
+    )
     private String name;
 
     @NotEmpty(message = VALIDATION_TYPE_NOT_EMPTY_MESSAGE, groups = {Read.class, Create.class, PutUpdate.class})
@@ -48,5 +57,8 @@ public class ProjectDto {
             groups = {Read.class, Create.class, PutUpdate.class})
     @Pattern(regexp = VALIDATION_TYPE_PATTERN, message = VALIDATION_TYPE_SIZE_MESSAGE,
             groups = PatchUpdate.class)
+    @Schema(
+            description = "type of project", example = "Super lux"
+    )
     private String type;
 }
--- IoTProject/src/main/java/be/jimsa/iotproject/ws/model/dto/ResponseDto.java
+++ IoTProject/src/main/java/be/jimsa/iotproject/ws/model/dto/ResponseDto.java
@@ -1,16 +1,32 @@
 package be.jimsa.iotproject.ws.model.dto;
 
-import lombok.AllArgsConstructor;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
-@AllArgsConstructor
 @Builder
+@Schema(
+        name = "App Response",
+        description = "This is a kind of 'ResponseDto'"
+)
 public class ResponseDto {
+
+    @Schema(
+            description = "What is the final result? It will be true if the process is finished without any faults, otherwise, it is false.",
+            example = "true"
+    )
     private boolean action;
+
+    @Schema(
+            description = "The time of response"
+    )
     private LocalDateTime timestamp;
+
+    @Schema(
+            description = "The result of the response, including boolean, object, list, and so on"
+    )
     private Object result;
 }
