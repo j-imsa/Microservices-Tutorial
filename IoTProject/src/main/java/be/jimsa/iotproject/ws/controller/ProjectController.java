d9284230aa71b87b305f211c1f9818c9c667f876
Iman Salehi <cse.isalehi@gmail.com>
2024-09-02 15:12:24 +0330

    update controller @Documents

--- IoTProject/src/main/java/be/jimsa/iotproject/ws/controller/ProjectController.java
+++ IoTProject/src/main/java/be/jimsa/iotproject/ws/controller/ProjectController.java
@@ -1,10 +1,13 @@
 package be.jimsa.iotproject.ws.controller;
 
+import be.jimsa.iotproject.config.document.annotation.*;
 import be.jimsa.iotproject.config.validation.annotation.ValidPublicId;
 import be.jimsa.iotproject.utility.constant.ProjectConstants;
 import be.jimsa.iotproject.ws.model.dto.ProjectDto;
 import be.jimsa.iotproject.ws.model.dto.ResponseDto;
 import be.jimsa.iotproject.ws.service.ProjectService;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
@@ -21,11 +24,19 @@ import java.util.List;
 @RequestMapping(path = "/v1/projects", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
 @Validated
+@Tag(
+        name = "Project endpoint",
+        description = "CRUD Rest APIs project details"
+)
 public class ProjectController {
 
     private final ProjectService projectService;
 
     @PostMapping
+    @PostRequestDocument(
+            summary = "Create a new project",
+            description = "Create a new project using POST method and getting request body"
+    )
     public ResponseEntity<ResponseDto> createProject(
             @Validated(ProjectDto.Create.class) @RequestBody ProjectDto projectDto
     ) {
@@ -42,19 +53,31 @@ public class ProjectController {
     }
 
     @GetMapping
+    @GetAllRequestDocument(
+            summary = "Read all projects",
+            description = "Read all projects using GET method"
+    )
     public ResponseEntity<ResponseDto> getProjects(
             @Positive(message = ProjectConstants.VALIDATION_PAGE_POSITIVE_INTEGER)
-            @RequestParam(defaultValue = ProjectConstants.VALIDATION_PAGE_DEFAULT_VALUE) int page,
+            @RequestParam(defaultValue = ProjectConstants.VALIDATION_PAGE_DEFAULT_VALUE)
+            @Parameter(example = "3")
+            int page,
             @Positive(message = ProjectConstants.VALIDATION_SIZE_POSITIVE_INTEGER)
-            @RequestParam(defaultValue = ProjectConstants.VALIDATION_SIZE_DEFAULT_VALUE) int size,
+            @RequestParam(defaultValue = ProjectConstants.VALIDATION_SIZE_DEFAULT_VALUE)
+            @Parameter(example = "25")
+            int size,
             @Pattern(regexp = ProjectConstants.VALIDATION_SORT_FIELD_PATTERN,
                     message = ProjectConstants.VALIDATION_SORT_FIELD_PATTERN_MESSAGE)
             @RequestParam(value = ProjectConstants.VALIDATION_SORT_FIELD,
-                    defaultValue = ProjectConstants.VALIDATION_SORT_FIELD_DEFAULT_VALUE) String sortField,
+                    defaultValue = ProjectConstants.VALIDATION_SORT_FIELD_DEFAULT_VALUE)
+            @Parameter(example = "name", description = "'name' or 'type'")
+            String sortField,
             @Pattern(regexp = ProjectConstants.VALIDATION_SORT_DIRECTION_PATTERN,
                     message = ProjectConstants.VALIDATION_SORT_DIRECTION_PATTERN_MESSAGE)
             @RequestParam(value = ProjectConstants.VALIDATION_SORT_DIRECTION,
-                    defaultValue = ProjectConstants.VALIDATION_SORT_DIRECTION_DEFAULT_VALUE) String sortDirection
+                    defaultValue = ProjectConstants.VALIDATION_SORT_DIRECTION_DEFAULT_VALUE)
+            @Parameter(example = "desc", description = "'asc' or 'desc'")
+            String sortDirection
     ) {
         List<ProjectDto> foundedlist = projectService.findAllProjects(page, size, sortField, sortDirection);
         return ResponseEntity
@@ -68,8 +91,12 @@ public class ProjectController {
     }
 
     @GetMapping("/{public_id}")
+    @GetAnObjectRequestDocument(
+            summary = "Read a project",
+            description = "Read a project using GET method and getting a valid public_id as a parameter"
+    )
     public ResponseEntity<ResponseDto> getAProject(
-            @ValidPublicId @PathVariable("public_id") String publicId
+            @PublicIdDocument @ValidPublicId @PathVariable("public_id") String publicId
     ) {
         ProjectDto foundedProjectDto = projectService.findAProject(publicId);
         return ResponseEntity
@@ -83,8 +110,12 @@ public class ProjectController {
     }
 
     @PutMapping("/{public_id}")
+    @PutRequestDocument(
+            summary = "Update a whole project",
+            description = "Update a project using PUT method and getting a valid public_id as a parameter and a new project as a request body"
+    )
     public ResponseEntity<ResponseDto> updateAProject(
-            @ValidPublicId @PathVariable("public_id") String publicId,
+            @PublicIdDocument @ValidPublicId @PathVariable("public_id") String publicId,
             @Validated(ProjectDto.PutUpdate.class) @RequestBody ProjectDto newProjectDto
     ) {
         ProjectDto updatedProjectDto = projectService.updateAProject(publicId, newProjectDto);
@@ -99,8 +130,12 @@ public class ProjectController {
     }
 
     @PatchMapping("/{public_id}")
+    @PatchRequestDocument(
+            summary = "Update a piece of a project",
+            description = "Update a piece of a project using PATCH method and getting a valid public_id as a parameter and a new values as request bodies"
+    )
     public ResponseEntity<ResponseDto> patchAProject(
-            @ValidPublicId @PathVariable("public_id") String publicId,
+            @PublicIdDocument @ValidPublicId @PathVariable("public_id") String publicId,
             @Validated(ProjectDto.PatchUpdate.class) @RequestBody ProjectDto newProjectDto
     ) {
         ProjectDto updatedProjectDto = projectService.patchAProject(publicId, newProjectDto);
@@ -115,8 +150,12 @@ public class ProjectController {
     }
 
     @DeleteMapping("/{public_id}")
+    @DeleteAnObjectRequestDocument(
+            summary = "Delete a project",
+            description = "Delete a project using DELETE method and getting a valid public_id as a parameter"
+    )
     public ResponseEntity<ResponseDto> removeAProject(
-            @ValidPublicId @PathVariable("public_id") String publicId
+            @PublicIdDocument @ValidPublicId @PathVariable("public_id") String publicId
     ) {
         boolean result = projectService.removeAProject(publicId);
         return ResponseEntity
@@ -130,6 +169,10 @@ public class ProjectController {
     }
 
     @DeleteMapping
+    @DeleteAllRequestDocument(
+            summary = "Delete all projects",
+            description = "Delete all projects using DELETE method"
+    )
     public ResponseEntity<ResponseDto> removeProjects() {
         boolean result = projectService.removeAllProjects();
         return ResponseEntity
